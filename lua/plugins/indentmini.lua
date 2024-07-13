70847b7bc3df320f6ac75ddf55d36e8aa389b72b
fsncps <fsncps@eml.cc>
2024-07-13 19:05:21 +0200

    LSP update

--- lua/plugins/indentmini.lua
+++
@@ -1,1 +0,0 @@
-return { "nvimdev/indentmini.nvim" }
--- lua/plugins/neoclip.lua
+++ lua/plugins/neoclip.lua
@@ -1,11 +1,11 @@
 return {
-	"AckslD/nvim-neoclip.lua",
-	requires = {
-		{ "nvim-telescope/telescope.nvim" },
-		{ "kkharji/sqlite.lua", module = "sqlite" },
-	},
-	config = function()
-		require("neoclip").setup()
-		require("telescope").load_extension("neoclip")
-	end,
+   "AckslD/nvim-neoclip.lua",
+   requires = {
+      { "nvim-telescope/telescope.nvim" },
+      { "kkharji/sqlite.lua" }, -- Ensure this matches the directory structure
+   },
+   config = function()
+      require("neoclip").setup()
+      require("telescope").load_extension("neoclip")
+   end,
 }
