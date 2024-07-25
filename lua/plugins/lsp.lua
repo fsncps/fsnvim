b332ab6134f14702547e32a88095170b359809e9
fsncps <fsncps@eml.cc>
2024-07-25 21:14:54 +0200

    fixed LSP

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,15 +6,23 @@ return {
       "WhoIsSethDaniel/mason-tool-installer.nvim",
       { "j-hui/fidget.nvim",       opts = {} },
       { "folke/neodev.nvim",       opts = {} },
+      -- Add nvim-cmp and related plugins
+      "hrsh7th/nvim-cmp",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "saadparwaiz1/cmp_luasnip",
+      "L3MON4D3/LuaSnip",
    },
    config = function()
       -- Set up Mason
       require('mason').setup()
       require('mason-lspconfig').setup({
-         ensure_installed = { 'lua_ls' }
+         ensure_installed = { 'lua_ls', 'pyright', 'bashls', 'texlab' }
       })
 
       local lspconfig = require('lspconfig')
+      local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
       -- Set up lua_ls with Mason
       lspconfig.lua_ls.setup({
@@ -42,17 +50,23 @@ return {
                },
             },
          },
+         capabilities = capabilities,
       })
 
-      -- Configure capabilities for autocompletion
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+      -- Set up other LSP servers
+      lspconfig.pyright.setup { capabilities = capabilities }
+      lspconfig.bashls.setup { capabilities = capabilities }
+      lspconfig.texlab.setup { capabilities = capabilities }
 
       -- Ensure additional tools are installed
       require("mason-tool-installer").setup({
          ensure_installed = {
-            "stylua",     -- Used to format Lua code
+            "stylua",      -- Used to format Lua code
+            "black",       -- Used to format Python code
+            "shfmt",       -- Used to format Shell scripts
+            "latexindent", -- Used to format LaTeX code
          },
       })
    end,
 }
+
