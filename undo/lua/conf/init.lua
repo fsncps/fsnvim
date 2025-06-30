e17df98564fe2ab95bdbc0c42a7c906cae97975e
fsncps <fsncps@eml.cc>
2025-06-30 12:33:24 +0200

    rm undo

--- undo/lua/conf/init.lua
+++
@@ -1,7 +0,0 @@
-vim.cmd("colorscheme catppuccin-macchiato")
-vim.cmd([[highlight MsgArea guibg=#24273a guifg=#7ce322]])
-vim.cmd([[highlight Pmenu guibg=#1e2030]])
-vim.cmd([[highlight PmenuSel guibg=#f5a97f guifg=#24273a]])
---
-vim.cmd([[highlight Normal guibg=#24273a]])
-Require_All("conf")
--- undo/lua/plugins/nui.lua
+++
@@ -1,4 +0,0 @@
-return {
-   'MunifTanjim/nui.nvim',
-   requires = 'nvim-lua/plenary.nvim' -- required dependency
-}
