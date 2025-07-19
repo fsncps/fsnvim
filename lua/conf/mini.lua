4b37fa2e2a8db5631b823f65046ad29deb4263aa
fsncps <fsncps@eml.cc>
2025-07-20 01:10:49 +0200

    mini move

--- lua/conf/mini.lua
+++ lua/conf/mini.lua
@@ -1,14 +1,14 @@
-require('mini.icons').setup()
-require('mini.move').setup({
-   mappings = {
-      left       = '<S-left>',
-      right      = '<S-right>',
-      down       = '<S-down>',
-      up         = '<S-up>',
+require("mini.icons").setup()
+require("mini.move").setup({
+	mappings = {
+		left = "<S-left>",
+		right = "<S-right>",
+		down = "<S-down>",
+		up = "<S-up>",
 
-      line_left  = '<S-left>',
-      line_right = '<S-right>',
-      line_down  = '<S-down>',
-      line_up    = '<S-up>',
-   }
+		line_left = "<S-left>",
+		line_right = "<S-right>",
+		line_down = "<S-down>",
+		line_up = "<S-up>",
+	},
 })
