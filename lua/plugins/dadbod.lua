4ab36cb998bd8b4bfdf4ece4dd7e587df4cb4430
fsncps <fsncps@eml.cc>
2025-07-20 01:59:10 +0200

    rm neotree and SQL stuff

--- lua/plugins/dadbod.lua
+++
@@ -1,9 +0,0 @@
-return {
-   "tpope/vim-dadbod",
-   dependencies = {
-      "kristijanhusak/vim-dadbod-ui",
-      "kristijanhusak/vim-dadbod-completion"
-   },
-   cmd = { "DB", "DBUI", "DBUIToggle", "DBUIAddConnection" },
-   keys = { { "<leader>db", "<cmd>DBUIToggle<CR>", desc = "Toggle DB UI" } }
-}
